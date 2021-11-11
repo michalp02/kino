bb9b0e3fae73896a4bd70c8fb2756edd1a5365df
Michał Pasierbski <szkolne@pasierbski.eu>
2021-11-12 00:25:03 +0100

    Stworzenie API

---
+++ Api/1.0/czyZalogowano.php
@@ -0,0 +1,14 @@
+<?php
+
+header("Content-type: text/plain");
+require '../sys/sys.php';
+if($kino->czyZalogowano())
+echo '{
+  "success": true,
+}';
+else 
+echo '{
+    "success": false,
+  }';
+
+?>
---
+++ Api/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Api</title>
+</head>
+<body>
+    <h1>Hej</h1>
+    <h2>Nie powinno być Cię na tej podstronie</h2>
+    <p><a href="./">Wróć na stronę główną</a></p>
+</body>
+</html>
